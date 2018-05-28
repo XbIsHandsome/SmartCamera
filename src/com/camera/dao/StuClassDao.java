a0d96bdec034fab831f60c038355133795c36055
XbIsHandsome <954009381@qq.com>
2018-05-28 16:44:54 +0800

    完成登录，实时监控的前端设计

---
+++ src/com/camera/dao/StuClassDao.java
@@ -0,0 +1,24 @@
+package com.camera.dao;
+
+import java.util.List;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import com.camera.action.StuClassAction;
+
+public class StuClassDao extends HibernateDaoSupport{
+
+	@SuppressWarnings("unchecked")
+	public List<StuClassAction> getAll() {
+		String hql = "from StuClass";
+		List<StuClassAction> list = this.getHibernateTemplate().find(hql);
+		if(list != null && list.size() > 0 ){
+			return list;
+		}
+		return null;
+	}
+
+	
+	
+	
+}
